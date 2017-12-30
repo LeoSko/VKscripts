ae587f7af01236c5293c59701f48399acee620b9
LeoSko <leosko94@gmail.com>
2017-12-31 02:51:11 +0300

    Added random wishes with message + refactoring

--- birthdays/script.py
+++ birthdays/script.py
@@ -1,6 +1,18 @@
 import vk
 from datetime import date, datetime, timedelta
 import locale
+from bs4 import BeautifulSoup
+import urllib3
+import random
+
+# Group to look for member in
+target_group_id 	 = 75494873
+days_before 		 = 3
+days_after 			 = 10
+# How long history t should load to try sending you user's photos
+photo_history_days 	 = 90
+# Who will receive the results of scripts
+receipants 			 = '12245505,349758'
 
 from access_token import access_token, group_access_token
 session = vk.Session(access_token=access_token)
@@ -9,12 +21,25 @@ api = vk.API(session, v='5.69')
 gapi = vk.API(group_session)
 locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
+# Get random wishes from site
+http = urllib3.PoolManager()
+page_idx = random.randint(1, 404)
+if page_idx == 1:
+	page_idx = ''
+else:
+	page_idx = '-' + str(page_idx)
+page_url = 'http://pozdravok.ru/pozdravleniya/den-rozhdeniya/proza{0}.htm'.format(page_idx)
+page = http.request('GET', page_url).data
+soup = BeautifulSoup(page, 'lxml')
+pars = soup.find_all('p')
+rpar = pars[random.randint(0, len(pars) - 1)]
+
 now = date.today()
 
-startDay = now - timedelta(days=3)
-endDay = now + timedelta(days=10)
+startDay = now - timedelta(days=days_before)
+endDay = now + timedelta(days=days_after)
 
-users = api.groups.getMembers(group_id=75494873, fields="bdate")['items']
+users = api.groups.getMembers(group_id=target_group_id, fields="bdate")['items']
 
 result_table = []
 has_birthday_today = False
@@ -46,12 +71,12 @@ for user in result_table:
 		for photo in all_photos:
 			photo_datetime = datetime.fromtimestamp(photo['date'])
 			photo_date = date(day=photo_datetime.day, month=photo_datetime.month, year=photo_datetime.year)
-			if photo_date > user.date - timedelta(days=90):
+			if photo_date > user.date - timedelta(days=photo_history_days):
 				photos.append(photo)
 		for photo in tagged_photos:
 			photo_datetime = datetime.fromtimestamp(photo['date'])
 			photo_date = date(day=photo_datetime.day, month=photo_datetime.month, year=photo_datetime.year)
-			if photo_date > user.date - timedelta(days=90):
+			if photo_date > user.date - timedelta(days=photo_history_days):
 				tphotos.append(photo)
 		photos.sort(key=lambda x: x['likes']['count'], reverse=True)
 		tphotos.sort(key=lambda x: x['likes']['count'], reverse=True)
@@ -64,11 +89,14 @@ for user in result_table:
 		this_user_string = '&#127881;' + this_user_string
 	result_strings.append(this_user_string)
 
-result_message = 'Сегодня ' + now.strftime('%d.%m.%Y') + \
-				'\nДни рождения с ' + startDay.strftime('%d.%m') + ' по ' + endDay.strftime('%d.%m') + \
-				'\n' + '\n'.join(result_strings)
+result_message = '''Сегодня {0}
+Дни рождения с {1} по {2}
+{3}
+Случайное пожелание:
+{4}
+Больше на: {5}'''.format(now.strftime('%d.%m.%Y'), startDay.strftime('%d.%m'), endDay.strftime('%d.%m'), '\n'.join(result_strings), rpar.text, page_url)
 
 result_attachments = ','.join(attachments)
-print(gapi.messages.send(user_ids='12245505,349758', message=result_message, attachment=result_attachments))
+print(gapi.messages.send(user_ids=receipants, message=result_message, attachment=result_attachments))
 #print(result_message)
 #print(result_attachments)
